546d2ba16d1b33b107134b633b51072af7c9972c
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-20 15:47:55 +0100

    Add project files.

---
+++ DataBaseAndORM/School.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseAndORM
+{
+    public class School
+    {
+        public string SchoolName { get; set; }
+        public string PrincipalFirstName { get; set; }
+        public string SchoolAddress { get; set; }
+        public int TotalStudents { get; set; }
+    }
+}

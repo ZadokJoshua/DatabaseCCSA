8fd86b2655e7eb481ac3d119f3da33681409e293
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-26 12:08:07 +0100

    Appointment

--- HospitalManagementSystem_LiveEvaluation2/Activities/Appointment.cs
+++ HospitalManagementSystem_LiveEvaluation2/Activities/Appointment.cs
@@ -6,6 +6,49 @@ namespace HospitalManagementSystem_LiveEvaluation2.Activities
     {
         public virtual Doctors Doctor { get; set; }
         public virtual string Condition { get; set; }
+        public virtual string Drugs { get; set; }
+
+        IDictionary<string, decimal> billBreakdom = new Dictionary<string, decimal>();
+
+
+        public static decimal TotalBills = 0;
+
+        public void AddBill(string details, decimal payment)
+        {
+            billBreakdom.Add(details, payment);
+            TotalBills += payment;
+        }
+
+
+        public void SubtractBill(string details)
+        {
+            if (billBreakdom.ContainsKey(details))
+            {
+                billBreakdom.Remove(details);
+                Console.WriteLine("Removed - " + details);
+            }
+            else
+            {
+                Console.WriteLine("Didn't find - " + details);
+            }
+        }
+
+        public decimal PrintBills()
+        {
+            return TotalBills;
+        }
+
+        public void AppointmentActivity()
+        {
+            Console.WriteLine("Your condition - " + Condition);
+            Console.WriteLine("Drugs delivered - " + Drugs);
+
+            // Print BillBreakdown
+            foreach (KeyValuePair<string, decimal> kvp in billBreakdom)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
+        }
 
     }
 }
--- HospitalManagementSystem_LiveEvaluation2/Models/Patients.cs
+++ HospitalManagementSystem_LiveEvaluation2/Models/Patients.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagementSystem_LiveEvaluation2.Activities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,11 @@ namespace HospitalManagementSystem_LiveEvaluation2.Models
         public virtual int HospitalNumber { get; set; }
         public virtual Doctors Doctor { get; set; }
         public virtual DateTime AppointmentTime { get; set; }
+
+        public virtual void BookAppointment()
+        {
+            var appointment = new Appointment();
+            appointment.AppointmentActivity();
+        }
     }
 }

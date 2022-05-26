e10845e044705dd607514f5586eb9b7712c44ced
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-26 13:04:56 +0100

    Sql

---
+++ HospitalManagementSystem_LiveEvaluation2/SqlQueries/SQLQuery1.sql
@@ -0,0 +1,16 @@
+--Create Table SchoolNew(
+--SchoolName VarChar,
+--PrincipalFirstName VarChar,
+--SchoolAddress VarChar,
+--TotalStudents Int,
+--)
+
+--INSERT INTO [dbo].[School] 
+--(
+--[SchoolName], 
+--[PrincipalFirstName], 
+--[SchoolAddress], 
+--[TotalStudents]) 
+--VALUES ('Desmond', 'Abdul', 'Ogun', 2003092);
+
+SELECT * FROM School Where TotalStudents = 2003092;
